a85f8f2e36c71d7a0a3854a8826f9c70f454f221
adebayo <kurdzsamuel@gmail.com>
2020-12-18 04:29:11 +0100

    cards and accordion added

--- components/Hero-Section.vue
+++ components/Hero-Section.vue
@@ -26,7 +26,7 @@
       </div>
     </div>
     <div
-      style="height:fit-content; overflow:hidden; border: 1px solid #56667F; font-size: 12px; padding:1px; width: 100%;">
+      style="height:fit-content; overflow: hidden; font-size: 12px; padding:1px; width: 100%;">
       <div style="height:40px; width: 100%;">
         <iframe src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=light&pref_coin_id=1505&invert_hover=" width="100%" height="36px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;">
         </iframe>
